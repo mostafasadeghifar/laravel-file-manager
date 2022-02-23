3142d5a91fa428028e12ad2f33ad134abd2c557c
mostafasadeghifar <msadeghifar1996@gmail.com>
2022-02-23 08:33:37 +0330

    remove-cdn-for-net

---
+++ resources/views/fmButton.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- wwwwwxsCSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'File Manager') }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/file-manager/css/file-manager.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12" id="fm-main-block">
+            <div id="fm"></div>
+        </div>
+    </div>
+</div>
+
+<!-- File manager -->
+<script src="{{ asset('vendor/file-manager/js/file-manager.js') }}"></script>
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // set fm height
+    document.getElementById('fm-main-block').setAttribute('style', 'height:' + window.innerHeight + 'px');
+
+    // Add callback to file manager
+    fm.$store.commit('fm/setFileCallBack', function(fileUrl) {
+      window.opener.fmSetLink(fileUrl);
+      window.close();
+    });
+  });
+</script>
+</body>
+</html>
+
